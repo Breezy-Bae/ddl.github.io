e1fd5ba53b37f2ec5881faa6494da4eddb68575d
Breezy-Bae <breezy.bae.bb19@gmail.com>
2025-06-06 02:41:55 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
-// src/main.tsx
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";  // ← import BrowserRouter
-import App from "./App.tsx";
-import "./index.css";
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/">
-    <App />
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
