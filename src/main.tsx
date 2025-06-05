23a953fe68a38cbb7a33bdc0f32ad629ee9083ab
Breezy-Bae <breezy.bae.bb19@gmail.com>
2025-06-06 02:38:56 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 // src/main.tsx
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";  // ← import BrowserRouter
 import App from "./App.tsx";
 import "./index.css";
 
